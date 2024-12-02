bc39bbef7b0ec7b10013799bbb5e1fd7a96fe9cf
Mayamba Simão <turmalrs3@outlook.com>
2024-12-02 20:19:20 +0000

    config fastapi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="Minha API",
+    description="Documentação da API",
+    version="1.0.0",
+    docs_url="/docs",  # URL do Swagger
+    redoc_url="/redoc",  # URL do ReDoc
+    openapi_url="/openapi.json"  # URL para o esquema OpenAPI
+    )
 
 app.include_router(user)
